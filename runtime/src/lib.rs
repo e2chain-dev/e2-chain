7e27e179217919870d7e987666b8bca50606898d
Chao Ma <mc8305@gmail.com>
2020-09-09 22:22:25 -0700

    change node-template to e2-chain

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -97,8 +97,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("node-template"),
-    impl_name: create_runtime_str!("node-template"),
+    spec_name: create_runtime_str!("e2-chain"),
+    impl_name: create_runtime_str!("e2-chain"),
     authoring_version: 1,
     spec_version: 1,
     impl_version: 1,
