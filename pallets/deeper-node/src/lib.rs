fc4bd826c9d6a9a36134551f490a0c77b8601bc2
Chao Ma <mc8305@gmail.com>
2020-09-11 17:59:05 -0700

    add helper functions: try_add_server, try_remove_server; add unregister; make sure server list and node country information is consistency during operations

--- pallets/deeper-node/src/lib.rs
+++ pallets/deeper-node/src/lib.rs
@@ -12,7 +12,7 @@ pub trait Trait: frame_system::Trait {
     type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
 }
 
-const MIN_LOCK_AMT: u64 = 60;
+const MIN_LOCK_AMT: u32 = 100;
 
 type BalanceOf<T> =
     <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
@@ -21,7 +21,8 @@ type BalanceOf<T> =
 #[derive(Decode, Encode, Default)]
 pub struct Node<AccountId> {
     account_id: AccountId,
-    ipv4: Vec<u8>, // IP will not be exposed in future version
+    ipv4: Vec<u8>,            // IP will not be exposed in future version
+    country: Option<Vec<u8>>, // country code, only server need this field
 }
 
 // events
@@ -32,9 +33,13 @@ decl_event!(
         //Balance = BalanceOf<T>,
     {
         // register node: AccountId, ipv4
-        RegisterNode(AccountId, Vec<u8>),
-        // register server: AccountId, country
-        RegisterServer(AccountId, Vec<u8>),
+        RegisterNode(AccountId, Vec<u8>, Option<Vec<u8>>),
+        UnregisterNode(AccountId),
+
+        // add account into a country's server list
+        ServerAdded(AccountId, Vec<u8>),
+        // remove account from a country's server list
+        ServerRemoved(AccountId, Vec<u8>),
     }
 );
 
@@ -54,38 +59,94 @@ decl_module! {
       fn deposit_event() = default;
 
       #[weight = 10_000]
-      pub fn register_device(origin, ip: Vec<u8>) -> DispatchResult {
+      pub fn register_device(origin, ip: Vec<u8>, country: Option<Vec<u8>>) -> DispatchResult {
           let sender = ensure_signed(origin)?;
           ensure!(ip.len() == 4, "IPv4 has 4 bytes");
           let node = Node {
               account_id: sender.clone(),
               ipv4: ip.clone(),
+              country: country.clone(),
           };
-          // TODO: lock some tokens
+          T::Currency::reserve(&sender,BalanceOf::<T>::from(MIN_LOCK_AMT))?;
           <DeviceInfo<T>>::insert(sender.clone(), node);
 
-          Self::deposit_event(RawEvent::RegisterNode(sender, ip));
+          Self::deposit_event(RawEvent::RegisterNode(sender, ip, country));
           Ok(())
       }
+
+      #[weight = 10_000]
+      pub fn unregister_device(origin) -> DispatchResult {
+          let sender = ensure_signed(origin)?;
+          ensure!(<DeviceInfo<T>>::contains_key(sender.clone()), "device not registered!");
+          let node = <DeviceInfo<T>>::get(sender.clone());
+          if !node.country.is_none() {
+              let country = node.country.unwrap();
+              let _ = Self::try_remove_server(&sender, &country);
+          }
+          <DeviceInfo<T>>::remove(sender.clone());
+          T::Currency::unreserve(&sender,BalanceOf::<T>::from(MIN_LOCK_AMT));
+          Self::deposit_event(RawEvent::UnregisterNode(sender));
+          Ok(())
+      }
+
 
       #[weight = 10_000]
       pub fn register_server(origin, country: Vec<u8>) -> DispatchResult {
           let sender = ensure_signed(origin)?;
           ensure!(<DeviceInfo<T>>::contains_key(sender.clone()),
               "sender device needs register first");
-          ensure!(country.len() == 1, "Country code has 1 byte");
+          ensure!(country.len() == 2, "Country code has 2 byte");
 
-          // TODO: think of more efficient way
-          let mut server_list = <ServersByCountry<T>>::get(country.clone());
-          let cloned_sender = sender.clone(); // for efficiency
-          for item in &server_list {
-              ensure!(*item != cloned_sender, "double registration not allowed!");
+          match Self::try_add_server(&sender, &country) {
+              Ok(_) => { // update country in the node field
+                  let mut node = <DeviceInfo<T>>::get(sender.clone());
+                  node.country = Some(country.clone());
+                  <DeviceInfo<T>>::insert(sender.clone(),node);
+              },
+              Err(err) => {
+                  return Err(err);
+              },
           }
-          server_list.push(cloned_sender);
-          <ServersByCountry<T>>::insert(country.clone(),server_list);
-          Self::deposit_event(RawEvent::RegisterServer(sender, country));
+
           Ok(())
       }
 
+      #[weight = 10_000]
+      pub fn unregister_server(origin, country: Vec<u8>) -> DispatchResult {
+          let sender = ensure_signed(origin)?;
+          ensure!(<DeviceInfo<T>>::contains_key(sender.clone()),
+              "sender device needs register first");
+          ensure!(country.len() == 2, "Country code has 2 byte");
+          let _ = Self::try_remove_server(&sender, &country);
+          Ok(())
+      }
   }
+}
+
+impl<T: Trait> Module<T> {
+    // try to remove an account from a country's server list if exists
+    fn try_remove_server(sender: &T::AccountId, country: &Vec<u8>) -> DispatchResult {
+        let mut server_list = <ServersByCountry<T>>::get(country.clone());
+        if let Some(index) = server_list.iter().position(|x| *x == sender.clone()) {
+            server_list.remove(index);
+            <ServersByCountry<T>>::insert(country.clone(), server_list);
+            Self::deposit_event(RawEvent::ServerRemoved(sender.clone(), country.clone()));
+        }
+        Ok(())
+    }
+
+    // try to add an account to a country's server list; no double add
+    fn try_add_server(sender: &T::AccountId, country: &Vec<u8>) -> DispatchResult {
+        let mut server_list = <ServersByCountry<T>>::get(country.clone());
+
+        let cloned_sender = sender.clone();
+        for item in &server_list {
+            ensure!(*item != cloned_sender, "double registration not allowed!");
+        }
+
+        server_list.push(cloned_sender);
+        <ServersByCountry<T>>::insert(country.clone(), server_list);
+        Self::deposit_event(RawEvent::ServerAdded(sender.clone(), country.clone()));
+        Ok(())
+    }
 }
